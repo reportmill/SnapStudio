22828c6e086e6d5f92bb91a0b251a794e2788198
Jeff Martin <jeff@reportmill.com>
2019-01-16 11:07:36 -0600

    Silly stuff.

---
+++ src/studio/apptools/BoneView.java
@@ -0,0 +1,24 @@
+package studio.apptools;
+import snap.gfx.*;
+import snap.view.*;
+
+/**
+ * A custom class.
+ */
+public class BoneView extends View {
+
+/**
+ * Override to paint joint and bone.
+ */
+public void paintFront(Painter aPntr)
+{
+    double w = getWidth(), h = getHeight(), jw = 2;
+
+    aPntr.setColor(Color.RED); aPntr.fill(new Ellipse(0,h/2-jw/2,jw,jw));
+    
+    Polygon pg = new Polygon();
+    pg.addPoint(jw+2, h/2-4); pg.addPoint(w,h/2-2); pg.addPoint(w,h/2+2); pg.addPoint(jw+2,h/2+4);
+    aPntr.fill(pg);
+}
+
+}
---
+++ src/studio/apptools/BoneViewTool.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2010, ReportMill Software. All rights reserved.
+ */
+package studio.apptools;
+import snap.gfx.Color;
+import studio.app.*;
+import snap.view.*;
+
+/**
+ * This class handles editing of RectViews.
+ */
+public class BoneViewTool <T extends BoneView> extends ViewTool <T> {
+    
+/**
+ * Returns a new instance of the shape class that this tool is responsible for.
+ */
+protected T newInstance()  { T rect = super.newInstance(); rect.setBorder(Color.BLACK, 1); return rect; }
+
+protected void initUI()  { }
+
+/**
+ * Updates the UI controls from the currently selected rectangle.
+ */
+public void resetUI()
+{
+    // Get selected rectangle (just return if null)
+    BoneView rect = getSelectedView(); if(rect==null) return;
+    
+    // Update RoundingThumb and RoundingText
+    //setViewValue("RoundingThumb", rect.getRadius());
+    //setViewValue("RoundingText", rect.getRadius());
+}
+
+/**
+ * Updates the currently selected rectangle from the UI controls.
+ */
+public void respondUI(ViewEvent anEvent)
+{
+    // Get the current rect view and rects list (just return if null)
+    BoneView rect = getSelectedView(); if(rect==null) return;
+    //List <RectView> rects = (List)getSelectedViews();
+
+    // Handle Rounding Radius Thumb & Text
+    /*if(anEvent.equals("RoundingThumb") || anEvent.equals("RoundingText")) {
+        //rect.undoerSetUndoTitle("Rounding Change");
+        float value = anEvent.getFloatValue();
+        for(RectView r : rects) {
+            r.setRadius(value);
+            if(r.getBorder()==null)
+                r.setBorder(Color.BLACK, 1);
+        }
+    }*/
+}
+
+/**
+ * Event handling - overridden to install cross-hair cursor.
+ */
+public void mouseMoved(ViewEvent anEvent)  { getEditor().setCursor(Cursor.CROSSHAIR); }
+
+/**
+ * Returns the class that this tool is responsible for.
+ */
+public Class getViewClass()  { return BoneView.class; }
+
+/**
+ * Returns the name to be presented to user.
+ */
+public String getWindowTitle()  { return "Rectangle Tool"; }
+
+}

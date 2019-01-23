9949fa21a2068d1358c2fb2d55593cb255c52f7f
Jeff Martin <jeff@reportmill.com>
2019-01-23 15:38:21 -0600

    SnapKit updates.

--- src/studio/app/GradientStopPicker.java
+++ src/studio/app/GradientStopPicker.java
@@ -181,7 +181,7 @@ public void deleteColorStop()
     removeStop(_selectedKnob);
     _selectedKnob = -1; // deselect
     resetComponents();
-    getEnv().runLater(() -> fireActionEvent());
+    getEnv().runLater(() -> fireActionEvent(null));
 }
 
 /**
@@ -293,7 +293,7 @@ protected void processEvent(ViewEvent anEvent)
         // A drag after this moves the new stop.
         if ((sindex<0) && _gradientRect.contains(pt)) {
             sindex = addStop(pt, null);
-            fireActionEvent();
+            fireActionEvent(anEvent);
         }
         
         // Create and run the pop-up menu
@@ -313,7 +313,7 @@ protected void processEvent(ViewEvent anEvent)
     else if(anEvent.isMouseDrag()) {
         if(_selectedKnob>=0) {
             adjustColorStop(_selectedKnob, anEvent.getPoint());
-            fireActionEvent();
+            fireActionEvent(anEvent);
         }
     }
     
@@ -345,7 +345,7 @@ protected void processEvent(ViewEvent anEvent)
             addStop(getStopOffset(_dragPoint), color);
             _dragPoint = null;
             resetComponents();
-            fireActionEvent();
+            fireActionEvent(anEvent);
             anEvent.dropComplete();
         } //else dtde.rejectDrop();
     }
@@ -401,7 +401,7 @@ private void colorWellPropertyChange(PropChange aPC)
         int which = _wells.indexOf(cwell); Color color = cwell.getColor();
         setStop(which, getStopOffset(which), color);
         repaint();
-        getEnv().runLater(() -> fireActionEvent());
+        getEnv().runLater(() -> fireActionEvent(null));
     }
 }
 
